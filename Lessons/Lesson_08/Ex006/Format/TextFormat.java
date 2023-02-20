32799da1202331cd74c77efbf6f399d3398a24b2
Liubov <ashka-08@mail.ru>
2023-02-20 17:02:28 +0500

    edit searchPeople, don't work

---
+++ Lessons/Lesson_08/Ex006/Format/TextFormat.java
@@ -0,0 +1,8 @@
+package Lesson_08.Ex006.Format;
+
+import java.io.Serializable;
+
+import Lesson_08.Ex006.Interface.Saveable;
+
+public abstract class TextFormat implements Saveable, Serializable {
+}
